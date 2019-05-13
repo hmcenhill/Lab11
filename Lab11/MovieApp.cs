c710af46e2c29833d3b76b5a5fd8032e428e306a
Hermann I McEnhill <hmcenhill@gmail.com>
2019-05-13 12:52:50 -0400

    Make display cleaner

--- Lab11/MovieApp.cs
+++ Lab11/MovieApp.cs
@@ -11,25 +11,43 @@ namespace Lab11
             var movieList = SeedDB.LoadMovies();
             do
             {
-                LookupMovies(movieList);
-            } while (Workflow.KeepGoing("Continue?"));
+                var foundMovies = LookupMovies(movieList);
+                DisplayMovies(foundMovies);
+            } while (Workflow.KeepGoing("\nContinue?"));
         }
 
-        private static void LookupMovies(List<Movie> movieList)
+        private static List<Movie> LookupMovies(List<Movie> movieList)
         {
             var input = GetUserInputCategory("What category are you interested in? ");
-            int movieCount = 0;
+            var foundMovies = new List<Movie>();
+
             foreach (var movie in movieList)
             {
                 if (movie.Genre == input)
                 {
-                    Console.WriteLine(movie.Title);
-                    movieCount++;
+                    foundMovies.Add(movie);
                 }
             }
-            Console.WriteLine($"{movieCount} movie(s) found");
+            return foundMovies;
         }
+        private static void DisplayMovies(List<Movie> foundMovies)
+        {
+            Console.Clear();
+            switch (foundMovies.Count)
+            {
+                case 1:
+                    Console.WriteLine($"{foundMovies.Count} {foundMovies[0].Genre.ToString()} movie found:\n");
+                    break;
+                default:
+                    Console.WriteLine($"{foundMovies.Count} {foundMovies[0].Genre.ToString()} movies found:\n");
+                    break;
+            }
+            foreach (var movie in foundMovies)
+            {
+                Console.WriteLine(movie.Title);
+            }
 
+        }
         private static Category GetUserInputCategory(string question)
         {
             Console.WriteLine(question);
