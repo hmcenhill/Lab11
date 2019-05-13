272b9e677b66267ae8b72d5630b5f6e1559cd74a
Hermann I McEnhill <hmcenhill@gmail.com>
2019-05-13 16:17:07 -0400

    Made prettier with tables

--- Lab11/MovieApp.cs
+++ Lab11/MovieApp.cs
@@ -15,7 +15,6 @@ namespace Lab11
                 DisplayMovies(foundMovies);
             } while (Workflow.KeepGoing("\nContinue?"));
         }
-
         private static List<Movie> LookupMovies(List<Movie> movieList)
         {
             var input = GetUserInputCategory("What category are you interested in? ");
@@ -42,11 +41,25 @@ namespace Lab11
                     Console.WriteLine($"{foundMovies.Count} {foundMovies[0].Genre.ToString()} movies found:\n");
                     break;
             }
+            int maxTitleLength = 0, maxYearLength = 0, maxRatingLength = 0, maxCategoryLength = 0;
             foreach (var movie in foundMovies)
             {
-                Console.WriteLine(movie.Title);
+                if (movie.Title.Length >= maxTitleLength) maxTitleLength = movie.Title.Length;
+                if (movie.Year.ToString().Length >= maxYearLength) maxYearLength = movie.Year.ToString().Length;
+                if (movie.Rating.ToString().Length >= maxRatingLength) maxRatingLength = movie.Rating.ToString().Length;
+                if (movie.Genre.ToString().Length >= maxCategoryLength) maxCategoryLength = movie.Genre.ToString().Length;
             }
-
+            int totalTableLength = maxTitleLength + maxYearLength + maxRatingLength + maxCategoryLength + 13;
+            string tableEdge = new string($"+{new string('-',totalTableLength - 2)}+");
+            Console.WriteLine(tableEdge);
+            foreach(var movie in foundMovies)
+            {
+                Console.WriteLine($"| {movie.Title}{new string(' ',maxTitleLength-movie.Title.Length)} " +
+                    $"| {movie.Year}{new string(' ',maxYearLength-movie.Year.ToString().Length)} " +
+                    $"| {movie.Rating}{new string(' ',maxRatingLength-movie.Rating.ToString().Length)} " +
+                    $"| {movie.Genre.ToString()}{new string(' ',maxCategoryLength-movie.Genre.ToString().Length)} |");
+            }
+            Console.WriteLine(tableEdge);
         }
         private static Category GetUserInputCategory(string question)
         {
